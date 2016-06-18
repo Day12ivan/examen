ce02535b2a639a8610678bd1ce5bfc475838e829
Day12ivan <herrera123daynor@gmail.com>
2016-06-18 10:56:54 -0400

    examen

---
+++ routes/objetoEjemplo.js
@@ -0,0 +1,14 @@
+var universidad ={
+	nombre:"unifranz",
+	direccion:"el alto",
+	carrera:"sistemas",
+	materia:"calculo",
+	horario:"trurno mañana",
+	}
+		for(i in universidad)
+		{
+			console.log(i +":"+	universidad[i]);
+
+		}
+
+
